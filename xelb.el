7cc5d1bbf61448b0280bc6719b38adee8130db5a
Chris Feng <chris.w.feng@gmail.com>
2016-08-14 12:26:53 +0800

    Bump version to 0.11

--- xelb.el
+++ xelb.el
@@ -4,7 +4,7 @@
 
 ;; Author: Chris Feng <chris.w.feng@gmail.com>
 ;; Maintainer: Chris Feng <chris.w.feng@gmail.com>
-;; Version: 0.10
+;; Version: 0.11
 ;; Package-Requires: ((emacs "24.4") (cl-generic "0.2"))
 ;; Keywords: unix
 ;; URL: https://github.com/ch11ng/xelb
